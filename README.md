4e486c0d304ca8d795cfa18fa21c564a9f813219
Dewa Putra Hernanda <dewananda089@gmail.com>
2022-11-24 14:45:45 +0700

    Setup awal

--- README.md
+++
@@ -1,1 +0,0 @@
-# tokopaimon-ecommerce
---
+++ tokopaimon/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import NavbarComponent from '../components/NavbarComponent'
+import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Home() {
+  return (
+    <NavbarComponent />
+  )
+}
